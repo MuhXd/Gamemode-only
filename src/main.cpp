0f24ac031c040d0659bd03b1b067290e449ccbc5
MuhXd <momo2010sofia@gmail.com>
2024-02-22 20:02:06 +0000

    new upd

--- src/main.cpp
+++ src/main.cpp
@@ -5,33 +5,42 @@ using namespace geode::prelude;
 #include <Geode/modify/GJBaseGameLayer.hpp>
 #include <Geode/binding/GJBaseGameLayer.hpp>
 
-void Switch(int sender) {
-    int tag = sender;
+void Switch(int gamemode, bool Plr) {
 	auto playLayer = PlayLayer::get();
-    auto player1 = playLayer->m_player1;
-    switch(tag) {
+	if (Plr) {
+    auto player = playLayer->m_player1;
+	}
+	else {
+		auto player = playLayer->m_player2;
+	}
+    switch(gamemode) {
         case 0:
-        player1->toggleFlyMode(false, false);
-        player1->toggleRollMode(false, false);
-        player1->toggleBirdMode(false, false);
-        player1->toggleDartMode(false, false);
-        player1->toggleRobotMode(false, false);
-        player1->toggleSpiderMode(false, false);
-        player1->toggleSwingMode(false, false);
+        player->toggleFlyMode(false, false);
+        player->toggleRollMode(false, false);
+        player->toggleBirdMode(false, false);
+        player->toggleDartMode(false, false);
+        player->toggleRobotMode(false, false);
+        player->toggleSpiderMode(false, false);
+        player->toggleSwingMode(false, false);
         break;
-        case 1: player1->toggleFlyMode(true, true); break; 
-        case 2: player1->toggleRollMode(true, true); break;
-        case 3: player1->toggleBirdMode(true, true); break;
-        case 4: player1->toggleDartMode(true, true); break;
-        case 5: player1->toggleRobotMode(true, true); break;
-        case 6: player1->toggleSpiderMode(true, true); break;
-        case 7: player1->toggleSwingMode(true, true); break;
+        case 1: player->toggleFlyMode(true, true); break; 
+        case 2: player->toggleRollMode(true, true); break;
+        case 3: player->toggleBirdMode(true, true); break;
+        case 4: player->toggleDartMode(true, true); break;
+        case 5: player->toggleRobotMode(true, true); break;
+        case 6: player->toggleSpiderMode(true, true); break;
+        case 7: player->toggleSwingMode(true, true); break;
 	}
 }
 
 class $modify(layer, GJBaseGameLayer) {
     void update(float delta) override {
         GJBaseGameLayer::update(delta);
-		Switch(0);
+		if Mod::get()->getSettingValue<bool>("Enabled") {
+		Switch(Mod::get()->getSettingValue<int64_t>("Gamemode"),true);
+		if (Mod::get()->getSettingValue<bool>("2nd-player")) {
+			Switch(Mod::get()->getSettingValue<int64_t>("Gamemode"),false);
+		};
+		}
 	}
 };
